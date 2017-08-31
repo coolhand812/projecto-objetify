fbcd97bab6922edb9b28e73de103c426bead3fe9
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2017-08-31 11:04:28 -0500

    Added library and player. Got it ready to reproduce songs

---
+++ Biblioteca.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Biblioteca
+{
+    private ArrayList<String> canciones;
+    private Reproductor reproductor;
+    public Biblioteca()
+    {
+        canciones =  new ArrayList<String>();
+        reproductor = new Reproductor();
+    }
+    
+    public void agregarCancion(String nombreArchivo)
+    {
+        canciones.add(nombreArchivo);
+    }
+    
+    public void reproducirCancion(int numeroCancion)
+    {
+        reproductor.reproducir(canciones.get(numeroCancion));
+    }
+}
---
+++ Reproductor.java
@@ -0,0 +1,8 @@
+public class Reproductor
+{
+    public void reproducir(String nombreArchivo)
+    {
+        //Simular la reproduccion
+        System.out.println("Reproduciendo: " + nombreArchivo);
+    }
+}
